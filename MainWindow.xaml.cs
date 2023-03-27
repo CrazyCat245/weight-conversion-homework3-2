72993b6a484601bccd9b94262a57285063616962
CrazyCat245 <125860812+CrazyCat245@users.noreply.github.com>
2023-03-27 23:34:11 +0800

    進一步修改

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,77 +24,133 @@ namespace weight_conversion
         {
             InitializeComponent();
         }
+        string strInput; // 字串型態的strInput變數
+        double douOutput; // double浮點數型態的douOutput變數
+        private void btnAllClear_Click(object sender, RoutedEventArgs e)
+        {
+            txtCM.Text = " ";
+            txtM.Text = " ";
+            txtKM.Text = " ";
+            txtIn.Text = " ";
+            txtFt.Text = " ";
+            txtYard.Text = " ";
+        }
 
         private void txtCM_KeyUp(object sender, KeyEventArgs e)//毫克
         {
-            double douCM; 
-            douCM = Convert.ToDouble(txtCM.Text); 
+            strInput = txtCM.Text;
 
-            txtM.Text = string.Format("{0:0.##########}", douCM * 0.001);
-            txtKM.Text = string.Format("{0:0.##########}", douCM * 0.000001);
-            txtIn.Text = string.Format("{0:0.##########}", douCM *0.000000001);
-            txtFt.Text = string.Format("{0:0.##########}", douCM * 0.000035274);
-            txtYard.Text = string.Format("{0:0.##########}", douCM * 0.0000022046);
+
+            if (double.TryParse(strInput, out douOutput) == true)
+            {
+                txtM.Text = string.Format("{0:0.##########}", douOutput * 0.001);
+            txtKM.Text = string.Format("{0:0.##########}", douOutput * 0.000001);
+            txtIn.Text = string.Format("{0:0.##########}", douOutput * 0.000000001);
+            txtFt.Text = string.Format("{0:0.##########}", douOutput * 0.000035274);
+            txtYard.Text = string.Format("{0:0.##########}", douOutput * 0.0000022046);
+            }
+            else
+            {
+                txtInfo.Text = "請輸入數字";
+                txtCM.Text = "";
+            }
         }
 
         private void txtM_KeyUp(object sender, KeyEventArgs e)//公克
         {
-            double douM;
-            douM = Convert.ToDouble(txtM.Text);
+            strInput = txtM.Text;
 
-            txtCM.Text = string.Format("{0:0.##########}", douM * 1000);
-            txtKM.Text = string.Format("{0:0.##########}", douM * 0.001);
-            txtIn.Text = string.Format("{0:0.##########}", douM * 0.000001);
-            txtFt.Text = string.Format("{0:0.##########}", douM * 0.0352739619);
-            txtYard.Text = string.Format("{0:0.##########}", douM * 0.0022046226);
+            if (double.TryParse(strInput, out douOutput) == true)
+            {
+                txtCM.Text = string.Format("{0:0.##########}", douOutput * 1000);
+            txtKM.Text = string.Format("{0:0.##########}", douOutput * 0.001);
+            txtIn.Text = string.Format("{0:0.##########}", douOutput * 0.000001);
+            txtFt.Text = string.Format("{0:0.##########}", douOutput * 0.0352739619);
+            txtYard.Text = string.Format("{0:0.##########}", douOutput * 0.0022046226);
+            }
+            else
+            {
+                txtInfo.Text = "請輸入數字";
+                txtM.Text = "";
+            }
         }
 
         private void txtKM_KeyUp(object sender, KeyEventArgs e)//公斤
         {
-            double douKM;
-            douKM = Convert.ToDouble(txtKM.Text);
+            strInput = txtKM.Text;
 
-            txtCM.Text = string.Format("{0:0.##########}", douKM * 1000000);
-            txtM.Text = string.Format("{0:0.##########}", douKM * 1000);
-            txtIn.Text = string.Format("{0:0.##########}", douKM * 0.001);
-            txtFt.Text = string.Format("{0:0.##########}", douKM * 35.27396195);
-            txtYard.Text = string.Format("{0:0.##########}", douKM * 2.2046226218);
+            if (double.TryParse(strInput, out douOutput) == true)
+            {
+
+                txtCM.Text = string.Format("{0:0.##########}", douOutput * 1000000);
+            txtM.Text = string.Format("{0:0.##########}", douOutput * 1000);
+            txtIn.Text = string.Format("{0:0.##########}", douOutput * 0.001);
+            txtFt.Text = string.Format("{0:0.##########}", douOutput * 35.27396195);
+            txtYard.Text = string.Format("{0:0.##########}", douOutput * 2.2046226218);
+            }
+            else
+            {
+                txtInfo.Text = "請輸入數字";
+                txtKM.Text = "";
+            }
         }
 
         private void txtIn_KeyUp(object sender, KeyEventArgs e)//公噸
         {
-            double douIn;
-            douIn = Convert.ToDouble(txtIn.Text);
+            strInput = txtIn.Text;
 
-            txtCM.Text = string.Format("{0:0.##########}", douIn * 1000000000);
-            txtM.Text = string.Format("{0:0.##########}", douIn * 1000000);
-            txtKM.Text = string.Format("{0:0.##########}", douIn * 1000);
-            txtFt.Text = string.Format("{0:0.##########}", douIn * 35273.96195);
-            txtYard.Text = string.Format("{0:0.##########}", douIn * 2204.6226218);
+            if (double.TryParse(strInput, out douOutput) == true)
+            {
+                txtCM.Text = string.Format("{0:0.##########}", douOutput * 1000000000);
+            txtM.Text = string.Format("{0:0.##########}", douOutput * 1000000);
+            txtKM.Text = string.Format("{0:0.##########}", douOutput * 1000);
+            txtFt.Text = string.Format("{0:0.##########}", douOutput * 35273.96195);
+            txtYard.Text = string.Format("{0:0.##########}", douOutput * 2204.6226218);
+            }
+            else
+            {
+                txtInfo.Text = "請輸入數字";
+                txtIn.Text = "";
+            }
         }
 
         private void txtFt_KeyUp(object sender, KeyEventArgs e)//盎司
         {
-            double douFt;
-            douFt = Convert.ToDouble(txtFt.Text);
+            strInput = txtFt.Text;
 
-            txtCM.Text = string.Format("{0:0.##########}", douFt * 28349523125);
-            txtM.Text = string.Format("{0:0.##########}", douFt * 28.349523125);
-            txtKM.Text = string.Format("{0:0.##########}", douFt * 0.0283495231);
-            txtIn.Text = string.Format("{0:0.##########}", douFt * 0.0000283495);
-            txtYard.Text = string.Format("{0:0.##########}", douFt * 0.0625);
+            if (double.TryParse(strInput, out douOutput) == true)
+            {
+                txtCM.Text = string.Format("{0:0.##########}", douOutput * 28349523125);
+            txtM.Text = string.Format("{0:0.##########}", douOutput * 28.349523125);
+            txtKM.Text = string.Format("{0:0.##########}", douOutput * 0.0283495231);
+            txtIn.Text = string.Format("{0:0.##########}", douOutput * 0.0000283495);
+            txtYard.Text = string.Format("{0:0.##########}", douOutput * 0.0625);
+            }
+            else
+            {
+                txtInfo.Text = "請輸入數字";
+                txtFt.Text = "";
+            }
         }
 
         private void txtYard_KeyUp(object sender, KeyEventArgs e)//磅
         {
-            double douYard;
-            douYard = Convert.ToDouble(txtYard.Text);
+            strInput = txtYard.Text;
 
-            txtCM.Text = string.Format("{0:0.##########}", douYard * 453592.37);
-            txtM.Text = string.Format("{0:0.##########}", douYard * 453.59237);
-            txtKM.Text = string.Format("{0:0.##########}", douYard * 0.45359237);
-            txtIn.Text = string.Format("{0:0.##########}", douYard * 0.0004535924);
-            txtFt.Text = string.Format("{0:0.##########}", douYard * 16);
+            if (double.TryParse(strInput, out douOutput) == true)
+            {
+
+                txtCM.Text = string.Format("{0:0.##########}", douOutput * 453592.37);
+            txtM.Text = string.Format("{0:0.##########}", douOutput * 453.59237);
+            txtKM.Text = string.Format("{0:0.##########}", douOutput * 0.45359237);
+            txtIn.Text = string.Format("{0:0.##########}", douOutput * 0.0004535924);
+            txtFt.Text = string.Format("{0:0.##########}", douOutput * 16);
+            }
+            else
+            {
+                txtInfo.Text = "請輸入數字";
+                txtYard.Text = "";
+            }
         }
     }
 }
